0a46aa29ad593558b89afeb96acd5958c4dfeb91
Shuai Zhou <shuaivzhou@berkeley.edu>
2023-03-28 02:10:08 -0400

    particle working

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -1,28 +1,37 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Resume from "../../resume.json";
+// import Typewriter from "typewriter-effect/dist/core";
 
 function Hero() {
   const typewriterStyle = {
-    color: "blue",
+    color: "orange",
   }
+  // new Typewriter('#typewriter', {
+  //   strings: ['Hello', 'World'],
+  //   autoStart: true,
+  //   loop: true,
+  // });
+  const tw = require("typewriterjs");
+  tw.startTypist("typewriterId", ["an engineer", "a product manager", "a developer", "a trader", "a language lover", "a content creator", "a Lineup Larry"]);
   return (
     <section className="hero is-dark is-fullheight has-bg-image">
       <div className="hero-head">
         <NavBar />
       </div>
+      <div className="column" style={{padding: "50px"}}></div>
       <div className="hero-body">
         <div className="container">
-          <p className="subtitle is-5 has-text-weight-light">I'm a</p>
-          <h1 className="title">{Resume.basics.label}</h1>
+          <p className="subtitle is-5 has-text-weight-light">My name is Victor Zhou.</p>
+          <h1 className="title">I am <span style={typewriterStyle} id="typewriterId"></span></h1>
+          <div></div>
           <h2 className="subtitle">
             {Resume.basics.location.region}, {Resume.basics.location.country}
           </h2>
         </div>
       </div>
-      <div className="hero-foot" style={{ paddingBottom: "20px" }}>
-        <div className="columns is-mobile">
-          <div className="column"></div>
+      <div className="hero-body" style={{ paddingBottom: "10px" }}>
+        <div className="container">
           {Resume.basics.profiles.map((value, index) => {
             return (
               <div key={index} className="column has-text-centered">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import { createRoot } from 'react-dom/client';
 import React from "react";
+// import ReactDOM from "react-dom";
 import App from "./App";
 
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />)
+
+// const tw = require("typewriterjs");
+// tw.startTypist("typewriterId", ["an engineer", "a product manager", "a developer", "a trader", "a language lover", "a content creator", "a Lineup Larry"]);
+
+const particlesJS = require("particles.js");
+particlesJS.load('particles-js', './particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
